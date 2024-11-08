a250a6a388ce341550a113aac296d3cf2b5f4fc9
Amro Alfien <ridzimeko@proton.me>
2024-11-08 02:10:52 +0000

    fix beberapa style

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    api: 'modern'
-                }
-            }
-        }
-    }
+  vite: {
+      css: {
+          preprocessorOptions: {
+              scss: {
+                  api: 'modern'
+              }
+          }
+      }
+  },
+
+  integrations: [icon()]
 });
